e7561e20a75fc60023d863522a82b3d2ddd4d4d7
Raymond John Hill <raymondjohnhill@gmail.com>
2012-09-04 22:33:03 -0400

    Added: Integers!

---
+++ integers.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+//create one variable that stores an int
+var one int
+
+//create two more variables that store ints
+var two, three int
+
+func main() {
+  //set the variables
+  one = 1
+  two = 2
+  three = 3
+
+  //print everything
+  fmt.Println(one,two,three)
+}
