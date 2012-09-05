c14a9debcbf39f53ecc106ad020bb87dff867241
Raymond John Hill <raymondjohnhill@gmail.com>
2012-09-05 14:14:25 -0400

    Edited: Integers!

--- integers.go
+++ integers.go
@@ -9,11 +9,18 @@ var one int
 var two, three int
 
 func main() {
-  //set the variables
-  one = 1
-  two = 2
-  three = 3
+	//set the variables
+	one = 1
+	two = 2
+	three = 3
 
-  //print everything
-  fmt.Println(one,two,three)
+	//print everything
+	fmt.Println(one,two,three)
 }
+
+//Instructions for running:
+//(assuming you have Go installed)
+//0.save this file as integers.go
+//1.open terminal or cmd
+//2.cd to the directory that contains this file
+//3.type 'go run integers.go'
