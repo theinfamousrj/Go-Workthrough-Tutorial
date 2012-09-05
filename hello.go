22ad75e93f803d63ac3f20f24ed67e21d9dc6ff8
Raymond John Hill <raymondjohnhill@gmail.com>
2012-09-04 21:49:06 -0400

    Added: Hello World!

---
+++ hello.go
@@ -0,0 +1,15 @@
+//Go likes to work in packages
+package main
+
+//This imports fmt which contains the Println() function
+import "fmt"
+
+//Typical main function
+func main() {
+
+  //This is the line that actually prints the message
+  fmt.Println("Hello world!")
+
+}
+
+//when you are done save this file and type 'go run hello.go'
