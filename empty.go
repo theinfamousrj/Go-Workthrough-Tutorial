9d63b0a8749b4dd2d20e1702c483841b02f75ebd
Raymond John Hill <raymondjohnhill@gmail.com>
2012-09-05 14:16:48 -0400

    Edited: Empty!

--- empty.go
+++ empty.go
@@ -8,7 +8,15 @@ var b1 bool
 
 func main() {
 
-  //print the empty variables
-  fmt.Println(one, b1)
+	//print the empty variables
+	//to see what we get from them!
+	fmt.Println(one, b1)
 
 }
+
+//Instructions for running:
+//(assuming you have Go installed)
+//0.save this file as empty.go
+//1.open terminal or cmd
+//2.cd to the directory that contains this file
+//3.type 'go run empty.go'
