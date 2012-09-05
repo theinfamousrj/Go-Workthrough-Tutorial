27f26a9192340f72889ef0d2036c1b0179f9e732
Raymond John Hill <raymondjohnhill@gmail.com>
2012-09-04 22:40:51 -0400

    Added: Booleans!

---
+++ booleans.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+//instantiate the variables
+var one, two int
+var b1, b2, b3 bool
+
+func main() {
+  
+  //set the variables
+  one = 1
+  two = 2
+  b1 = true
+  b2 = false
+  b3 = true
+
+  //print the variables
+  fmt.Println(one, two, b1, b2, b3)
+}
